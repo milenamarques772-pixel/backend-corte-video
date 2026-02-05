2622f6ad7c1f1a98f54098ebaba86499ea439d39
milenamarques772-pixel <milenamarques772@gmail.com>
2026-02-05 20:41:46 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Backend rodando 🚀");
+  res.send("Servidor rodando!");
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log("Servidor rodando");
+  console.log("Servidor rodando na porta " + PORT);
 });
