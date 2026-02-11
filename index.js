7951b677097757c7d0970a5b51372dfadf1ea46e
milenamarques772-pixel <milenamarques772@gmail.com>
2026-02-11 01:08:40 -0300

    configura porta para Render

--- index.js
+++ index.js
@@ -36,11 +36,12 @@ app.post("/cortar", upload.single("video"), (req, res) => {
     });
   });
 });
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Servidor rodando na porta " + PORT);
+  console.log(Servidor rodando na porta ${PORT});
 });
+
+
 
 
